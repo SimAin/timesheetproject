39808ab11bd4db7ddc9fc7a469bb71ed1aabe435
Paul Matthews <pauliamgiant@gmail.com>
2019-04-25 20:15:26 +0100

    timesheetPage

---
+++ login.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
+
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="css/login.css">
+</head>
+
+<body>
+    <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+    <div class="container">
+        <form class="form-signin">
+            <div class="text-center mb-4">
+                <!-- <img class="mb-4" src="" alt="" width="72" height="72"> -->
+                <i class="fab fa-angrycreative fa-7x" style="color: #32e0e0"></i>
+                <h1 class="h3 mb-3 font-weight-normal">FDM Time Sheet</h1>
+            </div>
+
+            <div class="form-label-group">
+                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required=""
+                    autofocus="">
+                <label for="inputEmail">Email address</label>
+            </div>
+
+            <div class="form-label-group">
+                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+                <label for="inputPassword">Password</label>
+            </div>
+
+            <div class="checkbox mb-3">
+                <label>
+                    <input type="checkbox" value="remember-me"> Remember me
+                </label>
+            </div>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+            <p class="mt-5 mb-3 text-muted text-center">© 2019</p>
+        </form>
+    </div>
+    <!-- Bootstrap JS -->
+    <script src="jquery-ui-1.12.1.custom/external/jquery/jquery.js"></script>
+    <script src="jquery-ui-1.12.1.custom/jquery-ui.js"></script>
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
---
+++ timesheet.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="css/timesheet.css">
+</head>
+
+<body>
+    <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+    <div class="container">
+  
+       <!--<form class="form-signin">-->
+       <form class="form-horizontal" action="">
+        
+            <div class="text-center mb-4">
+                <!-- <img class="mb-4" src="" alt="" width="72" height="72"> -->
+                <i class="fab fa-angrycreative fa-7x" style="color: #32e0e0"></i>
+                <h1 class="h3 mb-3 font-weight-normal">Timesheet</h1>
+            </div>
+
+
+            <!--<div class="checkbox mb-3">
+                <label>
+                    <input type="checkbox" value="remember-me"> Remember me
+                </label>
+            </div>-->
+            
+        
+        
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="hours">Monday</label>
+    
+    <div class="col-sm-10">
+      <input type="number" class="form-control" id="monday" placeholder="Prepopulated Data here">
+    </div>
+
+  </div>
+
+
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="hours">Tuesday</label>
+    <div class="col-sm-10">
+      <input type="number" class="form-control" id="tuesday" placeholder="Prepopulated Data here">
+    </div>
+  </div>
+
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="hours">Wednesday</label>
+    <div class="col-sm-10">
+      <input type="number" class="form-control" id="wednesday" placeholder="Prepopulated Data here">
+    </div>
+  </div>
+
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="hours">Thursday</label>
+    <div class="col-sm-10">
+      <input type="number" class="form-control" id="thursday" placeholder="Prepopulated Data here">
+    </div>
+  </div>
+
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="hours">Friday</label>
+    <div class="col-sm-10">
+      <input type="number" class="form-control" id="friday" placeholder="Prepopulated Data here">
+    </div>
+  </div>
+
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="hours">Saturday</label>
+    <div class="col-sm-10">
+      <input type="number" class="form-control" id="saturday" placeholder="Prepopulated Data here">
+    </div>
+  </div>
+
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="hours">Sunday</label>
+    <div class="col-sm-10">
+      <input type="number" class="form-control" id="Sunday" placeholder="Prepopulated Data here">
+    </div>
+  </div>
+
+  <button class="btn btn-lg btn-primary btn-block" type="submit">Submit</button>
+            <p class="mt-5 mb-3 text-muted text-center">© 2019</p>
+</form> 
+    </div>
+    <!-- Bootstrap JS -->
+    <script src="jquery-ui-1.12.1.custom/external/jquery/jquery.js"></script>
+    <script src="jquery-ui-1.12.1.custom/jquery-ui.js"></script>
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
